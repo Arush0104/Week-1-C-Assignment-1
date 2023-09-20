b3cbe65cb83a830e9903cbe15c23166b004b448a
Arush James <53053119+Arush0104@users.noreply.github.com>
2023-09-20 20:52:29 +0100

    Add files via upload

---
+++ Week 1 C++ Assignment 1/Q5.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float circumference;
+    float pi = 3.1415;
+    float radius = 10;
+    circumference= 2*pi*radius;
+    cout<<circumference;
+}
